ed5cb14321661d910a3e08ce923550150d9ccbef
Steven S <stevenjslyker@gmail.com>
2019-05-07 17:24:52 -0600

    made react app GitHub friendly

---
+++ build/precache-manifest.a3e89ff19586511474910b097f537c95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e80619d4532140b00a2337f0defc7eb8",
+    "url": "/HW13-React-R1/index.html"
+  },
+  {
+    "revision": "17d4ad07d6e5a79052e1",
+    "url": "/HW13-React-R1/static/css/main.152fbd7f.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/HW13-React-R1/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "17d4ad07d6e5a79052e1",
+    "url": "/HW13-React-R1/static/js/main.443af71e.chunk.js"
+  },
+  {
+    "revision": "9f0a14cd2b44d8c71bab",
+    "url": "/HW13-React-R1/static/js/runtime~main.18bd3aeb.js"
+  }
+]);
